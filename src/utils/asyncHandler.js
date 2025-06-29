059aab27253d19b4f425bf8f70161fed1aaa8669
spy729 <rishiagarwal729@gmail.com>
2025-06-30 01:35:19 +0530

    added custom api error and response

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+// --------------- hard way to create a function wrapper but more efficient ------
+
+const asyncHandler = (requestHandler) => {
+    (req ,res , next) => {
+        Promise.resolve(requestHandler(req , res , next)).
+        catch((err) => next(err));
+    }
+}
+
+
+
+// ----------this is an easy way to do so it is a type of function wrapper ---------
+
+// const asyncHandler = (fn) => async (req , res , next) => {
+//     try {
+//         await fn(req , res , next);  
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success : false , 
+//             message : error.message,
+//         })
+//     }
+// }
+
+export {asyncHandler}
